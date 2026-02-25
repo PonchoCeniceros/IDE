b7bfd88e0358bea73f495159600a8b2ac663beea
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2026-02-25 15:43:20 -0600

    [main] formatter para sql

--- lua/plugins/editor.formatting.lua
+++ lua/plugins/editor.formatting.lua
@@ -5,5 +5,12 @@ return {
     opts.formatters_by_ft.json = { "prettier" }
     opts.formatters_by_ft.jsonc = { "prettier" }
     opts.formatters_by_ft.xml = { "xmlformatter" }
+    -- npm install -g sql-formatter
+    opts.formatters_by_ft.sql = { "sql_formatter" }
+    opts.formatters.sql_formatter = {
+      command = "sql-formatter",
+      args = { "--language", "postgresql" },
+      stdin = true,
+    }
   end,
 }
